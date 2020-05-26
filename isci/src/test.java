a8326b651a98f2eb7aa90e72f4e8ad0fe4c35a90
İlyas <ilyaskrt19@gmail.com>
2020-05-26 21:42:58 +0300

    Update test.java

--- isci/src/test.java
+++ isci/src/test.java
@@ -6,12 +6,12 @@ public class test {
 	public static void main(String[] args) {
 		int sonuc;
 		Scanner giris= new Scanner(System.in);
-		iscisýnýfý i1 = new iscisinifi("ilyas","Kurt",60,7);
-		iscisýnýfý i2= new iscisinifi();
+		iscisınıfı i1 = new iscisınıfı("ilyas","Kurt",60,7);
+		iscisınıfı i2= new iscisınıfı();
 		System.out.println("Sirayla isim Soyisim Çaliþma saati ve Saatlik ücreti giriniz.");
 		i2.setAd(giris.nextLine());
 		i2.setSoyad(giris.nextLine());
-		i2.setCalýs_sa(giris.nextInt());
+		i2.setCalıs_sa(giris.nextInt());
 		i2.setSaat_üc(giris.nextInt());
 		System.out.println("Adý "+i2.getAd()+" Soyadi "+i2.getSoyad()+" Toplam ücret= "+i2.hesap(i2.getCalis_sa(),i2.getSaat_üc()));
 		System.out.println("Adý "+i1.getAd()+" Soyadi "+i1.getSoyad()+" Toplam ücret= "+i1.hesap(i1.getCalis_sa(),i1.getSaat_üc()));
